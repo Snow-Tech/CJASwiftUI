821360068923d12d3cd77c38ed58bea4a63b3f4e
Brian Michael <clay.mrx10@gmail.com>
2022-04-27 15:23:41 +0100

    Initial Commit

---
+++ CJASwiftUI/CJASwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CJASwiftUIApp.swift
+//  CJASwiftUI
+//
+//  Created by Brian Hashirama on 27/04/22.
+//
+
+import SwiftUI
+
+@main
+struct CJASwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
